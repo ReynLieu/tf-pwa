2da4b7736dd80b1866d39b2167fd702508be156e
liuyr lzuhep <liuyinrui16@mails.ucas.ac.cn>
2021-06-12 00:09:22 +0800

    update toy studies

--- spin_ana.py
+++ spin_ana.py
@@ -12,7 +12,7 @@ def spin_ana(Ntoy, null="Z0", fitloop=1, sfit=True, cfit=True, param_file=None):
     CdNLL = []
     for i in range(Ntoy):
         print(f"##### Start toy {i}: null {null} alternative {alternative}")
-        Sdnll, Cdnll = sig_test(sfit=True, cfit=True, null=null, alternative=alternative, param_file=param_file, fitloop=fitloop)
+        Sdnll, Cdnll = sig_test(sfit=sfit, cfit=cfit, null=null, alternative=alternative, param_file=param_file, fitloop=fitloop)
         print("$$$$$dnll:", Sdnll, Cdnll)
         SdNLL.append(Sdnll)
         CdNLL.append(Cdnll)
@@ -21,6 +21,6 @@ def spin_ana(Ntoy, null="Z0", fitloop=1, sfit=True, cfit=True, param_file=None):
     return SdNLL, CdNLL
 
 if __name__ == "__main__":
-    SdNLL, CdNLL = spin_ana(100, null="Z0", fitloop=3, cfit=False) # edit
+    SdNLL, CdNLL = spin_ana(100, null="Z0", fitloop=3, sfit=True, cfit=True) # edit
     print(f"########## deltaNLL for Sfit:\n{SdNLL}")
     print(f"########## deltaNLL for Cfit:\n{CdNLL}")
